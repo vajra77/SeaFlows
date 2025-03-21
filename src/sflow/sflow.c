30218b28e10af1739cac0ad10b6ff358b5e220bd
vajra77 <f.ferreri@namex.it>
2025-03-21 14:39:13 +0100

    format mac address

--- src/sflow/sflow.c
+++ src/sflow/sflow.c
@@ -385,15 +385,9 @@ storable_flow_t	*sflow_encode_flow_record(const flow_record_t *record, const uin
 
 	flow->timestamp = time(NULL);
 
-	snprintf(flow->dst_mac, 13, "%x:%x:%x:%x:%x:%x",
-		pkt->datalink->ethernet.destination_mac[0],
-		pkt->datalink->ethernet.destination_mac[1],
-		pkt->datalink->ethernet.destination_mac[2],
-		pkt->datalink->ethernet.destination_mac[3],
-		pkt->datalink->ethernet.destination_mac[4],
-		pkt->datalink->ethernet.destination_mac[5]);
+	snprintf(flow->dst_mac, 13, "%x", pkt->datalink->ethernet.destination_mac);
 
-	snprintf(flow->src_mac, 13, "%x%x%x%x%x%x",
+	snprintf(flow->src_mac, 13, "%02x%02x%02x%02x%02x%02x",
 		pkt->datalink->ethernet.source_mac[0],
 		pkt->datalink->ethernet.source_mac[1],
 		pkt->datalink->ethernet.source_mac[2],
