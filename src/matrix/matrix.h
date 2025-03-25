eb5b00415a02a3d0cd57989f26342dbf30e0b0bf
vajra77 <f.ferreri@namex.it>
2025-03-25 08:23:10 +0100

    moving to rrdcached functions

--- src/matrix/matrix.h
+++ src/matrix/matrix.h
@@ -5,6 +5,7 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <rrd.h>
 #include "sflow/sflow.h"
 
 
@@ -37,6 +38,6 @@ typedef struct matrix {
 void matrix_init(matrix_t *matrix);
 void matrix_destroy(matrix_t *matrix);
 void matrix_clear(matrix_t *matrix);
-void matrix_dump(matrix_t *matrix);
+void matrix_dump(matrix_t *matrix, const rrd_client_t *);
 void matrix_add_flow(matrix_t *matrix, const storable_flow_t *flow);
 #endif //MATRIX_H
