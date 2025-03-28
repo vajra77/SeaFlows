0761a41dbe3e8094898ae8fa2127116d6bb3f5d5
vajra77 <f.ferreri@namex.it>
2025-03-28 09:55:54 +0100

    debugging rrd

--- src/rrdtool/rrdtool.c
+++ src/rrdtool/rrdtool.c
@@ -137,25 +137,13 @@ int rrd_store_flow(const srcnode_t *src, const dstnode_t *dst) {
 
 int rrd_store_peer(const srcnode_t *src) {
 
-	char basename[32];
 	char pathname[256];
 	char filename[256];
 	int err = 0;
 
-	/* flow file */
-	sprintf(basename, "/data/rrd/peers/%s", src->mac);
-	sprintf(pathname, "%s/peer_%s.rrd", basename, src->mac);
-	sprintf(filename, "peers/%s/peer_%s.rrd", src->mac, src->mac);
-
-	if (access(basename, F_OK) != 0) {
-		if (mkdir(basename, 0755)) {
-			syslog(LOG_ERR, "Unable to create directory: %s", basename);
-			return err;
-		}
-		err = create_rrd(filename);
-
-		return err;
-	}
+	/* peer file */
+	sprintf(filename, "peers/peer_%s.rrd", src->mac);
+	sprintf(pathname, "/data/rrd/peers/%s.rrd", src->mac);
 
 	if (access(pathname, F_OK) != 0) {
 			err = create_rrd(filename);
