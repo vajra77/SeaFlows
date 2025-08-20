9b2f011c71c9ebe07cd84b05edd32e37a2b656a1
vajra77 <f.ferreri@namex.it>
2025-08-20 12:15:14 +0200

    troubleshooting data sharing beetween threads

--- src/broker/broker.c
+++ src/broker/broker.c
@@ -23,18 +23,15 @@ void* broker_thread(void *arg) {
 	syslog(LOG_INFO, "Starting broker[%d]", broker->id);
 
 	for (;;) {
-		sleep(5);
+		sleep(60);
 		bucket_dump_t *dump = bucket_flush(broker->bucket);
 		for (int k = 0; k < dump->size; k++) {
 			bucket_node_t *node = dump->nodes[k];
-			// rrdtool_store(node->src, node->dst, node->bytes4, node->bytes6);
-			syslog(LOG_INFO, "broker[%d]: %s => %s (%u, %u)",
-				broker->id, node->src, node->dst, node->bytes4, node->bytes6);
+			rrdtool_store(node->src, node->dst, node->bytes4, node->bytes6);
 			MEM_free(node);
 		}
 		MEM_free(dump);
 		pthread_testcancel();
-		return NULL;
 	}
 	return NULL;
 }
