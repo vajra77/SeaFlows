b44af47f9bf592f8f6dfb7e548aaa0db818fadd7
vajra77 <f.ferreri@namex.it>
2025-09-12 12:08:40 +0200

    testing RRD fetching

--- frontend/go/seaflows_exporter.go
+++ frontend/go/seaflows_exporter.go
@@ -7,6 +7,6 @@ import (
 func main() {
 
 	e := NewRRDExporter("/srv/rrd", 1.0)
-	e.GetFlow("daily", "c0d6824e32ef", "1c34da8fff0d")
-	fmt.Print(e)
+	result := e.GetFlow("daily", "c0d6824e32ef", "1c34da8fff0d")
+	fmt.Print(result)
 }
