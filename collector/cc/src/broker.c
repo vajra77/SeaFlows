16abb670e76dde8ae8aa8a9c283bacddef82122c
vajra77 <f.ferreri@namex.it>
2025-10-15 14:44:56 +0200

    removed malloc dependencies

--- collector/cc/src/broker.c
+++ collector/cc/src/broker.c
@@ -36,8 +36,8 @@ void* broker_thread(void *arg) {
     	bucket_flush(broker->bucket, &dump);
 
 		for (int k = 0; k < dump.size; k++) {
-			bucket_node_t *node = dump.nodes[k];
-			rrdtool_store(node->src, node->dst, node->bytes4, node->bytes6);
+			bucket_node_t node = dump.nodes[k];
+			rrdtool_store(node.src, node.dst, node.bytes4, node.bytes6);
 		}
 
 		pthread_testcancel();
