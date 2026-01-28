d8ea785dd8540847b1acb49177052cfb862cca4e
vajra77 <f.ferreri@namex.it>
2026-01-28 16:25:09 +0100

    sketched Go exporter API

--- exporter/go/main.go
+++ exporter/go/main.go
@@ -1,12 +1,68 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/http"
+	"strconv"
 )
 
 func main() {
+	exporter := NewRRDExporter("/srv/rrd", 1.0)
 
-	e := NewRRDExporter("/srv/rrd", 1.0)
-	result := e.GetFlow("daily", "c0d6824e32ef", "1c34da8fff0d")
-	fmt.Print(result)
+	// APIv2: GET /api/v2/flow
+	http.HandleFunc("/api/v2/flow", func(w http.ResponseWriter, r *http.Request) {
+		src := r.URL.Query().Get("src")
+		dst := r.URL.Query().Get("dst")
+		protoStr := r.URL.Query().Get("proto")
+		schedule := r.URL.Query().Get("schedule")
+
+		proto, _ := strconv.Atoi(protoStr)
+
+		values, err := exporter.GetFlowData(src, dst, proto, schedule)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(map[string]string{"error": "Unable to find RRD file"})
+			return
+		}
+
+		response := FlowResponse{
+			Source:      src,
+			Destination: dst,
+			Proto:       protoStr,
+			Schedule:    schedule,
+			Values:      values,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	})
+
+	// APIv2: GET /api/v2/peer
+	http.HandleFunc("/api/v2/peer", func(w http.ResponseWriter, r *http.Request) {
+		src := r.URL.Query().Get("src")
+		protoStr := r.URL.Query().Get("proto")
+		schedule := r.URL.Query().Get("schedule")
+
+		proto, _ := strconv.Atoi(protoStr)
+
+		values, err := exporter.GetPeerData(src, proto, schedule)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(map[string]string{"error": "Unable to find RRD file"})
+			return
+		}
+
+		response := FlowResponse{
+			Source:      src,
+			Destination: "all",
+			Proto:       protoStr,
+			Schedule:    schedule,
+			Values:      values,
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
--- exporter/go/rrd.go
+++ exporter/go/rrd.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"math"
+	"os"
+	"strings"
 	"time"
 
 	RRDTool "github.com/ziutek/rrd"
@@ -11,6 +15,14 @@ type RRDExporter struct {
 	gamma   float64
 }
 
+type FlowResponse struct {
+	Source      string    `json:"src"`
+	Destination string    `json:"dst"`
+	Proto       string    `json:"proto"`
+	Schedule    string    `json:"schedule"`
+	Values      []float64 `json:"values"`
+}
+
 func NewRRDExporter(rootDir string, gamma float64) *RRDExporter {
 	return &RRDExporter{
 		rootDir: rootDir,
@@ -18,20 +30,119 @@ func NewRRDExporter(rootDir string, gamma float64) *RRDExporter {
 	}
 }
 
-func (e *RRDExporter) GetRootDir() string {
-	return e.rootDir
-}
+func (e *RRDExporter) GetFlowData(srcMac string, dstMac string, proto int, schedule string) ([]float64, error) {
+	rrdFile := e.rootDir + "/flows/" + srcMac + "/" + "flow_" + srcMac + "_to_" + dstMac + ".rrd"
 
-func (e *RRDExporter) GetGamma() float64 {
-	return e.gamma
-}
+	var start time.Time
+	var stepDuration time.Duration
+	var totalValues []float64
+	now := time.Now()
 
-func (e *RRDExporter) GetFlow(schedule string, src string, dst string) []float64 {
-
-	rrdFile := e.rootDir + "/flows/" + src + "/" + "flow_" + src + "_to_" + dst + ".rrd"
-	result, err := RRDTool.Fetch(rrdFile, "AVG", time.Now(), time.Now(), 300)
-	if err != nil {
-		return result.Values()
+	switch schedule {
+	case "weekly", "week", "w":
+		start = now.AddDate(0, 0, -7) // last week
+		stepDuration = 1800 * time.Second
+	case "monthly", "month", "m":
+		start = now.AddDate(0, -1, 0) // last month
+		stepDuration = 7200 * time.Second
+	case "yearly", "year", "y":
+		start = now.AddDate(-1, 0, 0) // last year
+		stepDuration = 86400 * time.Second
+	case "daily", "day", "d":
+		fallthrough
+	default:
+		start = now.AddDate(0, 0, -1) // last 24 hours
+		stepDuration = 300 * time.Second
 	}
-	return []float64{}
+
+	result, err := RRDTool.Fetch(rrdFile, "AVG", start, now, stepDuration)
+	if err != nil {
+		return nil, err
+	}
+
+	allValues := result.Values()
+
+	dsIndex := 0
+	if proto == 6 {
+		dsIndex = 1
+	}
+
+	numIntervals := len(allValues) / 2
+	totalValues = make([]float64, numIntervals)
+
+	for i := 0; i < numIntervals; i++ {
+		// Calcola l'indice corretto nell'array piatto
+		val := allValues[i*2+dsIndex]
+		totalValues[i] = val
+	}
+
+	return result.Values(), nil
+}
+
+func (e *RRDExporter) GetPeerData(peerMac string, proto int, schedule string) ([]float64, error) {
+	peerDir := e.rootDir + "/flows/" + peerMac
+
+	// read all files in directory
+	files, err := os.ReadDir(peerDir)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read peer directory: %v", err)
+	}
+
+	var totalValues []float64
+	var start time.Time
+	var stepDuration time.Duration
+	now := time.Now()
+
+	switch schedule {
+	case "weekly", "week", "w":
+		start = now.AddDate(0, 0, -7)
+		stepDuration = 1800 * time.Second
+	case "monthly", "month", "m":
+		start = now.AddDate(0, -1, 0)
+		stepDuration = 7200 * time.Second
+	case "yearly", "year", "y":
+		start = now.AddDate(-1, 0, 0)
+		stepDuration = 86400 * time.Second
+	default:
+		start = now.AddDate(0, 0, -1)
+		stepDuration = 300 * time.Second
+	}
+
+	for _, file := range files {
+		if !file.IsDir() && strings.HasSuffix(file.Name(), ".rrd") {
+			rrdPath := peerDir + "/" + file.Name()
+
+			result, err := RRDTool.Fetch(rrdPath, "AVG", start, now, stepDuration)
+			if err != nil {
+				continue // skip corrupted or errored files
+			}
+
+			allValues := result.Values()
+
+			dsIndex := 0
+			if proto == 6 {
+				dsIndex = 1
+			}
+
+			numIntervals := len(allValues) / 2
+			if totalValues == nil {
+				totalValues = make([]float64, numIntervals)
+			}
+
+			for i := 0; i < numIntervals; i++ {
+				// Calcola l'indice corretto nell'array piatto
+				val := allValues[i*2+dsIndex]
+
+				if !math.IsNaN(val) {
+					totalValues[i] += val
+				}
+			}
+		}
+	}
+
+	if totalValues == nil {
+		return nil, fmt.Errorf("no data for peer %s", peerMac)
+	}
+
+	return totalValues, nil
 }
