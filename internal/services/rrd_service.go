4f079b5ceb6ed080d2192d175ca5cb6041c33f76
vajra77 <f.ferreri@namex.it>
2026-02-23 13:45:26 +0100

    fixed RRD/RRA definitions

--- internal/services/rrd_service.go
+++ internal/services/rrd_service.go
@@ -164,8 +164,8 @@ func (s *rrdService) createRRDFile(dir, path string) error {
 	args := []string{
 		"create", path,
 		"--step", fmt.Sprintf("%d", s.step),
-		"DS:bytes4:GAUGE:600:U:U",
-		"DS:bytes6:GAUGE:600:U:U",
+		"DS:bytes4:ABSOLUTE:600:0:U",
+		"DS:bytes6:ABSOLUTE:600:0:U",
 		"RRA:AVERAGE:0.5:1:600",
 		"RRA:AVERAGE:0.5:6:700",
 		"RRA:AVERAGE:0.5:24:775",
@@ -173,7 +173,7 @@ func (s *rrdService) createRRDFile(dir, path string) error {
 		"RRA:MAX:0.5:1:600",
 		"RRA:MAX:0.5:6:700",
 		"RRA:MAX:0.5:24:775",
-		"RRA:MAX:0.5:444:797",
+		"RRA:MAX:0.5:288:797",
 	}
 	cmd := exec.Command("rrdtool", args...)
 
