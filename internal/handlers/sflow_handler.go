93caff030c82dcd5c3cc88855735ccaa035db363
vajra77 <f.ferreri@namex.it>
2026-04-03 16:20:22 +0200

    fixed issue with double counting

--- internal/handlers/sflow_handler.go
+++ internal/handlers/sflow_handler.go
@@ -98,6 +98,9 @@ func (h *sFlowHandler) worker(packetChan <-chan []byte) {
 		now := time.Now().Unix()
 
 		for _, sample := range dgram.Samples {
+			if sample.InputIf == 0 && sample.OutputIf != 0 {
+				continue // ignore egress-only packets
+			}
 			for _, record := range sample.Records {
 				if record.DataFormat == 1 { // Raw Packet Header
 					var flow *models.SflowData
